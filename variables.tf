f2f8639c85611f2f1e0cf976fb004bf422e2e525
myjinho <31680613+myjinho@users.noreply.github.com>
2023-04-04 11:58:22 +1000

    Delete vars used by user auth only

--- variables.tf
+++ variables.tf
@@ -2,20 +2,17 @@
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 
 variable "app_name" {
-  default     = "oci-function-cicd"
+  default     = "DevOps"
   description = "Application name. Will be used as prefix to identify resources, such as Function, VCN, DevOps, and others"
 }
 
 variable "release" {
   description = "Reference Architecture Release (OCI Architecture Center)"
-  default     = "1.0"
+  default     = "1.1"
 }
 
 variable "execute_deployment" {
